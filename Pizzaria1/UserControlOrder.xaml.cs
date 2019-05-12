ff9c7dbf54504563b0b8657795fa8b970c014f18
Mickey <mkproject.u@gmail.com>
2019-05-12 03:23:12 +0300

    backup 12,05,2019 3:22 completed UserControlOrder withnot button "In Cart"

--- Pizzaria1/UserControlOrder.xaml.cs
+++ Pizzaria1/UserControlOrder.xaml.cs
@@ -35,21 +35,34 @@ namespace Pizzaria1
         {
             page = 1;
             First_Textblock.Text = "Chipolo";
-            Second_Textblock.Margin = new Thickness(274, 96, 436, 440);
+            First_Textblock.Margin = new Thickness(92, 96, 618, 440.8);
             Second_Textblock.Text = "Dolche";
             Second_Textblock.Margin = new Thickness(274, 96, 436, 440);
             Third_Textblock.Text = "Emilia";
-            Second_Textblock.Margin = new Thickness(274, 96, 436, 440);
+            Third_Textblock.Margin = new Thickness(459, 96, 251, 440.8);
             fourty_Textblock.Text = "Hawai";
-            Second_Textblock.Margin = new Thickness(274, 96, 436, 440);
+            fourty_Textblock.Margin = new Thickness(646, 96, 64, 440.8);
             fifth_Textblock.Text = "Julien";
-            Second_Textblock.Margin = new Thickness(274, 96, 436, 440);
+            fifth_Textblock.Margin = new Thickness(99, 289, 616, 247.8);
             sixth_Textblock.Text = "Margarita";
-            Second_Textblock.Margin = new Thickness(274, 96, 436, 440);
+            sixth_Textblock.Margin = new Thickness(266, 289, 434, 247.8);
             seventh_Textblock.Text = "Manhattan";
-            Second_Textblock.Margin = new Thickness(274, 96, 436, 440);
+            seventh_Textblock.Margin = new Thickness(450, 289, 249, 247.8);
             eighth_Textblock.Text = "Peperoni";
-            Second_Textblock.Margin = new Thickness(274, 96, 436, 440);
+            eighth_Textblock.Margin = new Thickness(636, 289, 64, 247.8);
+
+
+
+
+
+            FirstImage.Source = new BitmapImage(new Uri("Images/chipolo.jpg", UriKind.Relative));
+            SecondImage.Source = new BitmapImage(new Uri("Images/dolche.jpg", UriKind.Relative));
+            ThirdImage.Source = new BitmapImage(new Uri("Images/emilia.jpg", UriKind.Relative));
+            fourthImage.Source = new BitmapImage(new Uri("Images/hawai.jpg", UriKind.Relative));
+            fifthImage.Source = new BitmapImage(new Uri("Images/julien.jpg", UriKind.Relative));
+            sixthImage.Source = new BitmapImage(new Uri("Images/Margarita.jpg", UriKind.Relative));
+            seventhImage.Source = new BitmapImage(new Uri("Images/munhaten.jpg", UriKind.Relative));
+            eighthImage.Source = new BitmapImage(new Uri("Images/peperoni.jpg", UriKind.Relative));
 
         }
 
@@ -57,7 +70,7 @@ namespace Pizzaria1
         {
             page = 2;
             First_Textblock.Text = "BigKing";
-            Second_Textblock.Margin = new Thickness(92, 96, 618, 440.8);
+            First_Textblock.Margin = new Thickness(92, 96, 618, 440.8);
             Second_Textblock.Text = "Cheaseburger";
             Second_Textblock.Margin = new Thickness(254, 96, 436, 440);
             Third_Textblock.Text = " X Cheaseburger";
@@ -72,6 +85,20 @@ namespace Pizzaria1
             seventh_Textblock.Margin = new Thickness(442, 289, 249, 247.8);
             eighth_Textblock.Text = "Vopper";
             eighth_Textblock.Margin = new Thickness(646, 289, 64, 247.8);
+
+
+
+            FirstImage.Source = new BitmapImage(new Uri("burgers/big king.png", UriKind.Relative));
+            SecondImage.Source = new BitmapImage(new Uri("burgers/cheaseburger def.png", UriKind.Relative));
+            ThirdImage.Source = new BitmapImage(new Uri("burgers/double cheaseburger.png", UriKind.Relative));
+            fourthImage.Source = new BitmapImage(new Uri("burgers/double vopper.png", UriKind.Relative));
+            fifthImage.Source = new BitmapImage(new Uri("burgers/gamburger.png", UriKind.Relative));
+            sixthImage.Source = new BitmapImage(new Uri("burgers/vopper + chease.png", UriKind.Relative));
+            seventhImage.Source = new BitmapImage(new Uri("burgers/vopper junior.png", UriKind.Relative));
+            eighthImage.Source = new BitmapImage(new Uri("burgers/vopper.png", UriKind.Relative));
+
+
+
 
         }
 
@@ -79,42 +106,69 @@ namespace Pizzaria1
         {
             page = 3;
             First_Textblock.Text = "7up";
-            Second_Textblock.Margin = new Thickness(92, 96, 618, 440.8);
+            First_Textblock.Margin = new Thickness(97, 96, 618, 440.8);
             Second_Textblock.Text = "Americano";
-            Second_Textblock.Margin = new Thickness(254, 96, 436, 440);
+            Second_Textblock.Margin = new Thickness(257, 96, 436, 440);
             Third_Textblock.Text = " Black tea";
-            Third_Textblock.Margin = new Thickness(432, 96, 251, 440.8);
+            Third_Textblock.Margin = new Thickness(447, 96, 251, 440.8);
             fourty_Textblock.Text = "Green tea";
-            fourty_Textblock.Margin = new Thickness(641, 96, 64, 440.8);
+            fourty_Textblock.Margin = new Thickness(635, 96, 64, 440.8);
             fifth_Textblock.Text = "Cappuccino";
-            fifth_Textblock.Margin = new Thickness(84, 289, 616, 247.8);
+            fifth_Textblock.Margin = new Thickness(82, 289, 616, 247.8);
             sixth_Textblock.Text = "Latte";
-            sixth_Textblock.Margin = new Thickness(255, 289, 434, 247.8);
+            sixth_Textblock.Margin = new Thickness(280, 289, 434, 247.8);
             seventh_Textblock.Text = "Mirinda";
-            seventh_Textblock.Margin = new Thickness(442, 289, 249, 247.8);
+            seventh_Textblock.Margin = new Thickness(460, 289, 249, 247.8);
             eighth_Textblock.Text = "Pepsi";
-            eighth_Textblock.Margin = new Thickness(646, 289, 64, 247.8);
+            eighth_Textblock.Margin = new Thickness(648, 289, 64, 247.8);
+
+
+
+
+            FirstImage.Source = new BitmapImage(new Uri("drinks/7up.png", UriKind.Relative));
+            SecondImage.Source = new BitmapImage(new Uri("drinks/americano.png", UriKind.Relative));
+            ThirdImage.Source = new BitmapImage(new Uri("drinks/black tea.png", UriKind.Relative));
+            fourthImage.Source = new BitmapImage(new Uri("drinks/green tea.png", UriKind.Relative));
+            fifthImage.Source = new BitmapImage(new Uri("drinks/kapuchino.png", UriKind.Relative));
+            sixthImage.Source = new BitmapImage(new Uri("drinks/latte.png", UriKind.Relative));
+            seventhImage.Source = new BitmapImage(new Uri("drinks/mirinda.png", UriKind.Relative));
+            eighthImage.Source = new BitmapImage(new Uri("drinks/pepsi.png", UriKind.Relative));
+
         }
 
         private void Other_Btn_Click(object sender, RoutedEventArgs e)
         {
             page = 4;
-            First_Textblock.Text = "BigKing";
-            Second_Textblock.Margin = new Thickness(92, 96, 618, 440.8);
-            Second_Textblock.Text = "Cheaseburger";
-            Second_Textblock.Margin = new Thickness(254, 96, 436, 440);
-            Third_Textblock.Text = " X Cheaseburger";
+            First_Textblock.Text = "Chery Pie";
+            First_Textblock.Margin = new Thickness(83, 96, 624, 440.8);
+            Second_Textblock.Text = "Chocolate donut";
+            Second_Textblock.Margin = new Thickness(248, 96, 436, 440);
+            Third_Textblock.Text = "Chocolate shake";
             Third_Textblock.Margin = new Thickness(432, 96, 251, 440.8);
-            fourty_Textblock.Text = "X Vopper";
-            fourty_Textblock.Margin = new Thickness(641, 96, 64, 440.8);
-            fifth_Textblock.Text = "Gamburger";
-            fifth_Textblock.Margin = new Thickness(84, 289, 616, 247.8);
-            sixth_Textblock.Text = "Chease Vopper";
-            sixth_Textblock.Margin = new Thickness(255, 289, 434, 247.8);
-            seventh_Textblock.Text = "Junior Vopper";
-            seventh_Textblock.Margin = new Thickness(442, 289, 249, 247.8);
-            eighth_Textblock.Text = "Vopper";
-            eighth_Textblock.Margin = new Thickness(646, 289, 64, 247.8);
+            fourty_Textblock.Text = "KitKia icecream";
+            fourty_Textblock.Margin = new Thickness(625, 96, 64, 440.8);
+            fifth_Textblock.Text = "Twix icecream";
+            fifth_Textblock.Margin = new Thickness(77, 289, 616, 247.8);
+            sixth_Textblock.Text = "icecream";
+            sixth_Textblock.Margin = new Thickness(267, 289, 434, 247.8);
+            seventh_Textblock.Text = "strawberry donut";
+            seventh_Textblock.Margin = new Thickness(434, 289, 249, 247.8);
+            eighth_Textblock.Text = "strawberry shake";
+            eighth_Textblock.Margin = new Thickness(619, 289, 64, 247.8);
+
+
+
+            FirstImage.Source = new BitmapImage(new Uri("other/chery pie.png", UriKind.Relative));
+            SecondImage.Source = new BitmapImage(new Uri("other/chocolate donut.png", UriKind.Relative));
+            ThirdImage.Source = new BitmapImage(new Uri("other/chocolate shake.png", UriKind.Relative));
+            fourthImage.Source = new BitmapImage(new Uri("other/icecream kitkat.png", UriKind.Relative));
+            fifthImage.Source = new BitmapImage(new Uri("other/icecream twix.png", UriKind.Relative));
+            sixthImage.Source = new BitmapImage(new Uri("other/icecream.png", UriKind.Relative));
+            seventhImage.Source = new BitmapImage(new Uri("other/strawberry donut.png", UriKind.Relative));
+            eighthImage.Source = new BitmapImage(new Uri("other/strawberry shake.png", UriKind.Relative));
+
+
+
         }
 
     }
